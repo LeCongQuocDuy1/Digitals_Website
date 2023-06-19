16a38cca02c9b6cf2d6fe0f8fd66ac834b8b6572
NEWUSER <duy9117@gmai.com>
2023-06-19 22:19:37 +0700

    create header duy

--- client/src/ultils/icons.js
+++ client/src/ultils/icons.js
@@ -5,8 +5,13 @@ import {
     FaInstagram,
     FaGoogle,
     FaPinterest,
+    FaPhoneAlt,
 } from "react-icons/fa";
 import { BiChevronDown } from "react-icons/bi";
+import { HiMail } from "react-icons/hi";
+import { FiHeart } from "react-icons/fi";
+import { BsFillHandbagFill } from "react-icons/bs";
+import { AiFillCaretDown } from "react-icons/ai";
 
 const icons = {
     FaMoneyBillAlt,
@@ -16,6 +21,11 @@ const icons = {
     FaInstagram,
     FaGoogle,
     FaPinterest,
+    FaPhoneAlt,
+    HiMail,
+    FiHeart,
+    BsFillHandbagFill,
+    AiFillCaretDown,
 };
 
 export default icons;
