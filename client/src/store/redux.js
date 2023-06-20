0bdbb4035bdc8a89b92b98c9e79d13615a1dc3de
NEWUSER <duy9117@gmai.com>
2023-06-20 15:26:51 +0700

    update sidebar with api redux toolkit

--- client/src/store/redux.js
+++ client/src/store/redux.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import appSlice from "./appSlice";
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        app: appSlice,
+    },
 });
