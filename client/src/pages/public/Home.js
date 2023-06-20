e18b2dd5e04943969e4a8e7f1838cb1b79e6d9dc
NEWUSER <duy9117@gmai.com>
2023-06-20 16:26:38 +0700

    update sidebar 1 fix conflict

--- client/src/pages/public/Home.js
+++ client/src/pages/public/Home.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "../../components/Sidebar";
 import Banner from "../../components/Banner";
+import { apiGetProducts } from "../../apis/";
 
 const Home = () => {
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const response = await apiGetProducts();
+            console.log(response);
+        };
+        fetchProducts();
+    }, []);
+
     return (
         <div className="w-main flex gap-[24px] items-start mt-[20px] mx-auto">
             <div className="w-[25%] max-h-[480px]">
